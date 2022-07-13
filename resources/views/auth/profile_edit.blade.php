1b9db9493356b86822187ee0b2ec98d7fe62bfa8
西澤未乗 <>
2022-07-13 22:34:08 +0900

    プロフィールページ更新しました

--- resources/views/auth/profile_edit.blade.php
+++ resources/views/auth/profile_edit.blade.php
@@ -12,21 +12,51 @@
 
     {{ Form::label('kana','Kana') }}
     {{ Form::text('kana',$auth->kana,['class'=>'form_controls']) }}
+        @foreach ($errors->get("kana") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
 
     {{ Form::label('username','Username') }}
     {{ Form::text('username',$auth->username,['class'=>'form_controls']) }}
+        @foreach ($errors->get("username") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
 
     {{ Form::label('email','Email') }}
     {{ Form::text('email',$auth->email,['class'=>'form_controls']) }}
+        @foreach ($errors->get("email") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
 
     {{ Form::label('password','New Password') }}
     {{ Form::password('password',['class'=>'form_controls','value'=>encrypt($auth->password)]) }}
+        @foreach ($errors->get("password") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
 
     {{ Form::label('bio','Bio') }}
     {{ Form::text('bio',$auth->bio,['class'=>'form_controls']) }}
+        @foreach ($errors->get("bio") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
 
     {{ Form::label('image','Image') }}
     {{ Form::file('image',['class'=>'form_controls','value'=>$auth->image]) }}
+        @foreach ($errors->get("image") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
 
     {{Form::submit('更新する',['class'=>'btn btn-success'])}}
 </div>
