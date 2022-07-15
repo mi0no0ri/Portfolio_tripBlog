851d03a7c5f0d78af8d42055baab5429f36a531a
西澤未乗 <>
2022-07-15 22:21:36 +0900

    プロフィールのバリデーション更新しました。

---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required|max:20',
+            'kana' => 'required|max:40',
+            'email' => 'required|max:40',
+            'password' => 'nullable|max:20|alpha_num',
+            'bio' => 'required|max:200',
+            'image' => 'image|file|mimes:jpg,png,bmp,gif,svg',
+        ];
+    }
+    public function message()
+    {
+        return [
+            'username.required' => '必須項目です',
+            'username.max' => '最大20文字で入力してください。',
+            'kana.required' => '必須項目です',
+            'kana.max' => '最大40文字で入力してください。',
+            'email.required' => '必須項目です',
+            'email.max' => '最大40文字で入力してください。',
+            'password.max' => '最大40文字で入力してください。',
+            'password.alpha_num' => 'アルファベットと数字で入力してください。',
+            'bio.required' => '必須項目です',
+            'bio.max' => '最大200文字で入力してください。',
+            'image.image' => '指定されたファイルが画像ではありません。',
+            'image.mimes' => '指定された拡張子（JPG、PNG、BMP、GIF、SVG）ではありません。',
+        ];
+    }
+}
--- resources/views/auth/profile_edit.blade.php
+++ resources/views/auth/profile_edit.blade.php
@@ -10,78 +10,54 @@
     {!! Form::hidden('id',$auth->id) !!}
     {{csrf_field()}}
 
-<<<<<<< HEAD
-    {{ Form::label('kana','Kana') }}
-    {{ Form::text('kana',$auth->kana,['class'=>'form_controls']) }}
+    <div class="form_content">
+        {{ Form::label('kana','Kana',['class' => 'profile_edit_tag']) }}
+        {{ Form::text('kana',$auth->kana,['class'=>'form_controls']) }}
         @foreach ($errors->get("kana") as $error)
             <div>
                 <strong>{{ $error }}</strong>
             </div>
         @endforeach
-
-    {{ Form::label('username','Username') }}
-    {{ Form::text('username',$auth->username,['class'=>'form_controls']) }}
-        @foreach ($errors->get("username") as $error)
-            <div>
-                <strong>{{ $error }}</strong>
-            </div>
-        @endforeach
-
-    {{ Form::label('email','Email') }}
-    {{ Form::text('email',$auth->email,['class'=>'form_controls']) }}
-        @foreach ($errors->get("email") as $error)
-            <div>
-                <strong>{{ $error }}</strong>
-            </div>
-        @endforeach
-
-    {{ Form::label('password','New Password') }}
-    {{ Form::password('password',['class'=>'form_controls','value'=>encrypt($auth->password)]) }}
-        @foreach ($errors->get("password") as $error)
-            <div>
-                <strong>{{ $error }}</strong>
-            </div>
-        @endforeach
-
-    {{ Form::label('bio','Bio') }}
-    {{ Form::text('bio',$auth->bio,['class'=>'form_controls']) }}
-        @foreach ($errors->get("bio") as $error)
-            <div>
-                <strong>{{ $error }}</strong>
-            </div>
-        @endforeach
-
-    {{ Form::label('image','Image') }}
-    {{ Form::file('image',['class'=>'form_controls','value'=>$auth->image]) }}
-        @foreach ($errors->get("image") as $error)
-            <div>
-                <strong>{{ $error }}</strong>
-            </div>
-        @endforeach
-=======
-    <div class="form_content">
-        {{ Form::label('kana','Kana',['class' => 'profile_edit_tag']) }}
-        {{ Form::text('kana',$auth->kana,['class'=>'form_controls']) }}
     </div>
 
     <div class="form_content">
         {{ Form::label('username','Username',['class' => 'profile_edit_tag']) }}
         {{ Form::text('username',$auth->username,['class'=>'form_controls']) }}
+        @foreach ($errors->get("username") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
     </div>
 
     <div class="form_content">
         {{ Form::label('email','Email',['class' => 'profile_edit_tag']) }}
         {{ Form::text('email',$auth->email,['class'=>'form_controls']) }}
+        @foreach ($errors->get("email") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
     </div>
 
     <div class="form_content">
         {{ Form::label('password','New Password',['class' => 'profile_edit_tag']) }}
         {{ Form::password('password',['class'=>'form_controls','value'=>encrypt($auth->password)]) }}
+        @foreach ($errors->get("password") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
     </div>
 
     <div class="form_content">
         {{ Form::label('bio','Bio',['class' => 'profile_edit_tag']) }}
         {{ Form::textarea('bio',$auth->bio,['class' => 'profile_bio']) }}
+        @foreach ($errors->get("bio") as $error)
+            <div>
+                <strong>{{ $error }}</strong>
+            </div>
+        @endforeach
     </div>
 
     <div class="form_content">
@@ -97,7 +73,6 @@
             </div>
         </div>
     </div>
->>>>>>> main
 
     <div class="send">
         {{Form::submit('更新する',['class'=>'send_btn'])}}
